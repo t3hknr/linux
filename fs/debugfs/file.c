03f6a33984865f2f8f9731a14d6c4434dc23a411
Michał Winiarski <michal.winiarski@intel.com>
2018-02-08 11:45:35 +0100

    debugfs

--- fs/debugfs/file.c
+++ fs/debugfs/file.c
@@ -206,6 +206,10 @@ FULL_PROXY_FUNC(unlocked_ioctl, long, filp,
 		PROTO(struct file *filp, unsigned int cmd, unsigned long arg),
 		ARGS(filp, cmd, arg));
 
+FULL_PROXY_FUNC(splice_read, ssize_t, filp,
+		PROTO(struct file *filp, loff_t *ppos, struct pipe_inode_info *pipe, size_t len, unsigned int flags),
+		ARGS(filp, ppos, pipe, len, flags));
+
 static __poll_t full_proxy_poll(struct file *filp,
 				struct poll_table_struct *wait)
 {
@@ -258,6 +262,8 @@ static void __full_proxy_fops_init(struct file_operations *proxy_fops,
 		proxy_fops->poll = full_proxy_poll;
 	if (real_fops->unlocked_ioctl)
 		proxy_fops->unlocked_ioctl = full_proxy_unlocked_ioctl;
+	if (real_fops->splice_read)
+		proxy_fops->splice_read = full_proxy_splice_read;
 }
 
 static int full_proxy_open(struct inode *inode, struct file *filp)
